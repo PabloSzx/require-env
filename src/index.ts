c736a6a3d5b63a709138edfa1f3e557553dde9c7
PabloSzx <pablosaez1995@gmail.com>
2020-07-13 00:51:29 -0400

    v3.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-export default <T extends string[]>(...envVariablesArray: T): Record<T[number], string> => {
-  const envVariables: Record<T[number], string> = {} as Record<T[number], string>;
+import "colors";
+
+export function requireEnv<T extends string[]>(
+  ...envVariablesArray: T
+): Record<T[number], string> {
+  const envVariables: Record<T[number], string> = {} as Record<
+    T[number],
+    string
+  >;
 
   const missingVariables: string[] = [];
   for (const v of envVariablesArray) {
-    if (!process.env[v]) {
+    const envV = process.env[v];
+    if (!envV) {
       missingVariables.push(v);
       continue;
     }
-    envVariables[v] = process.env[v];
+    envVariables[v] = envV;
   }
 
   if (missingVariables.length) {
-    console.error("Environment variables needed:", missingVariables);
-    throw new Error("Environment variables missing");
+    const error = Error(
+      (
+        "Environment variables missing: ".black +
+        "[".black +
+        ' "'.black +
+        missingVariables.map((v) => v.red.underline.bold).join('", "'.black) +
+        '" '.black +
+        "]".black
+      ).bgWhite
+    );
+
+    Error.captureStackTrace(error, requireEnv);
+
+    throw error;
   }
+
   return envVariables;
-};
+}
+
+export default requireEnv;
---
+++ test.ts
@@ -0,0 +1,3 @@
+import { requireEnv } from "./";
+
+requireEnv("ASD", "ZXC");
